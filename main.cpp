ae46b8f8f7627c7a8de6a5ce9ecdea9ea50461de
Egor Kolyuzhnov <egorfairman@gmail.com>
2023-03-07 00:59:05 +0700

    Final version

--- main.cpp
+++ main.cpp
@@ -15,22 +15,22 @@ double f(int n, double tau) {
 
 int main(int argc, char* argv[]) {
     if (argc != 6) {
-        std::cout << "Usage: ./main Nx Ny Nt Sx Sy" << std::endl;
+        std::cout << "Usage: ./main Nx Ny Nt" << std::endl;
         return EXIT_FAILURE;
     }
 
-    const int nx = std::stoi(argv[1]);
-    const int ny = std::stoi(argv[2]);
-    const int nt = std::stoi(argv[3]);
-    const int sx = std::stoi(argv[4]);
-    const int sy = std::stoi(argv[5]);
+    int nx = std::stoi(argv[1]);
+    int ny = std::stoi(argv[2]);
+    int nt = std::stoi(argv[3]);
+    int sx = std::stoi(argv[4]);
+    int sy = std::stoi(argv[5]);
 
     const auto start = std::chrono::high_resolution_clock::now();
 
-    const double xa = 0.0;
-    const double xb = 4.0;
-    const double ya = 0.0;
-    const double yb = 4.0;
+    constexpr double xa = 0.0;
+    constexpr double xb = 4.0;
+    constexpr double ya = 0.0;
+    constexpr double yb = 4.0;
 
     const double hx = (xb - xa) / (nx - 1);
     const double hy = (yb - ya) / (ny - 1);
@@ -39,7 +39,6 @@ int main(int argc, char* argv[]) {
     const double hyrec = 1 / (2 * hy * hy);
 
     const double tau = (nx <= 1000 && ny <= 1000) ? 0.01 : 0.001;
-    const double tau2 = tau * tau;
 
     int prevIndex = 0;
     int currIndex = 1;
@@ -58,29 +57,39 @@ int main(int argc, char* argv[]) {
     }
 
     for (int i = 0; i < nt; i++) {
+        double maxElement = 0;
         for (int y = 1; y < ny - 1; y++) {
             for (int x = 1; x < nx - 1; x++) {
-                const int index = y * nx + x;
-                const double uc = u[currIndex][index];
-                const double ur = u[currIndex][index + 1];
-                const double ul = u[currIndex][index - 1];
-                const double ut = u[currIndex][index + nx];
-                const double ud = u[currIndex][index - nx];
-                const double pc = p[index];
-                const double pd = p[index - nx];
-                const double pdl = p[index - nx - 1];
-                const double pl = p[index - 1];
+                int index = y * nx + x;
+                double uc = u[currIndex][index];
+                double ur = u[currIndex][y * nx + x + 1];
+                double ul = u[currIndex][y * nx + x - 1];
+                double ut = u[currIndex][(y + 1) * nx + x];
+                double ud = u[currIndex][(y - 1) * nx + x];
+                double pc = p[index];
+                double pd = p[(y - 1) * nx + x];
+                double pdl = p[(y - 1) * nx + x - 1];
+                double pl =p[y * nx + x - 1];
+                double fij = 0;
+                if (x == sx && y == sy) {
+                    fij = f(i, tau);
+                }
 
-                u[prevIndex][index] = 2 * uc - u[prevIndex][index] + tau2 * (
-                        ((ur - uc) * (pd + pc) + (ul - uc) * (pdl + pl)) * hxrec +
+                u[prevIndex][index] = 2 * uc - u[prevIndex][index] + (tau * tau) * (
+                        fij + ((ur - uc) * (pd + pc) + (ul - uc) * (pdl + pl)) * hxrec +
                                 ((ut - uc) * (pl + pc) + (ud - uc) * (pdl + pd)) * hyrec
                         );
+
+                if (u[prevIndex][index] > maxElement) {
+                    maxElement = u[prevIndex][index];
+                }
             }
         }
 
-        u[prevIndex][sy * nx + sx] += tau2 * f(i, tau);
-
         std::swap(prevIndex, currIndex);
+
+        std::cout << i << std::endl;
+        std::cout << maxElement << std::endl;    
     }
 
     const auto end = std::chrono::high_resolution_clock::now();
@@ -89,7 +98,7 @@ int main(int argc, char* argv[]) {
     std::cout << duration.count() / 1000.0 << "s" << std::endl;
 
     FILE* file = std::fopen("./main.dat", "w");
-    std::fwrite(&u[currIndex][0], sizeof(double), nx * ny, file);
+    std::fwrite(&u[prevIndex][0], sizeof(double), nx * ny, file);
     std::fclose(file);
 
     return EXIT_SUCCESS;
