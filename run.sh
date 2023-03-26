25a4c2528f8a784f41b803cae96b59a24272fa0b
Egor Kolyuzhnov <egorfairman@gmail.com>
2023-03-26 13:27:41 +0100

    Added simple parallel main

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-g++ -O3 -march=native -o lab1 "main1.cpp"
-./lab1 500 500 500 1 1 
+g++ -O3 -march=native -o lab1 --std=c++20 "parallel_main.cpp"
+./lab1 500 500 500 1 1 $1
 gnuplot script.txt
 feh main.dat.png
