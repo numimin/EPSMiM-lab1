86852bd874f470dd53710eeb8b06b552fb94d32b
Egor Kolyuzhnov <egorfairman@gmail.com>
2023-04-01 16:43:42 +0100

    Updated user info

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-g++ -O3 -march=native -o lab1 --std=c++20 "atomic.cpp"
-./lab1 500 500 500 1 1 $1
+g++ -O3 -march=native -o lab4 --std=c++20 "atomic.cpp"
+./lab4 500 500 500 1 1 $1
 gnuplot script.txt
 feh main.dat.png
